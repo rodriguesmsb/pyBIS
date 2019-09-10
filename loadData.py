3d0433baa68de08178a0ab1f3d44296c00f19039
rodriguesmsb <rodriguesmsb@gmail.com>
2019-09-10 08:01:24 -0400

    Insert AIH

--- loadData.py
+++ loadData.py
@@ -12,28 +12,39 @@ def set_data(dataset):
     elif dataset == "SIM":
         path = "/home/rodriguesmsb/Documents/Data_lake/Raw/Bancos_SIM/CSV_format/"
         pattern = "DO*[A-Z][A-Z]"
+    elif dataset == "AIH":
+        path = "/home/rodriguesmsb/Documents/Data_lake/Raw/Bancos_AIH/CSV_format/"
+        pattern = "RD*[A-Z][A-Z]"
     return([path, pattern])
 
 
 #Define function to point files
 def select_files(dataset, state = False, years = False, extension = ".csv"):
     """
-        This function is used to create a list of datasets based
+        This function is used to create a list of datasets based on one of
+        the brazilian information system e.g. AIH, SINASC, SINAN, SIM
     """
     path = dataset[0]
     pattern = dataset[1]
     results = {}
-    try:
+    if re.search(r"_AIH", path):
         for year in years:
             if pattern:
-                files = gb.glob(path + pattern + "*" + str(year) + "*" + extension)
-            else:
-                files = gb.glob(path + "*" + extension)
+                files = gb.glob(path + pattern + str(year)[2:4] + "*" + extension)
             results.update({year:files})
         return(results)
-    except:
-        if pattern:
+    else:
+        try:
+            for year in years:
+                if pattern:
+                    files = gb.glob(path + pattern + "*" + str(year) + "*" + extension)
+                else:
+                    files = gb.glob(path + "*" + extension)
+                results.update({year:files})
+            return(results)
+        except:
+            if pattern:
                 files = gb.glob(path + pattern + "*" + extension)
-        else:
-            files = gb.glob(path + "*" + extension)
-    return(files)
+            else:
+                files = gb.glob(path + "*" + extension)
+            return(files)
