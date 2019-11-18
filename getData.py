63dfd0a21223ab43b7bcb3897dd9de7a961a67b6
MORENO DE SOUZA RODRIGUES <rodriguesmsb@gmail.com>
2019-11-18 15:22:09 -0400

    Add files via upload
    Update getData

---
+++ getData.py
@@ -0,0 +1,43 @@
+import ftplib as ftp
+import glob as gb
+
+class dataSus:
+
+    #Start the class with main directory of the databases
+    def __init__(self, page = "ftp.datasus.gov.br"):
+        self.data_sus = ftp.FTP(page)
+        self.data_sus.login()
+        self.data_sus.cwd("/dissemin/publicos")
+
+
+    #Crete a method to choose specific data set
+    def select_data(self, data):
+        data = data.upper()
+
+
+        #Point to the correct directory for every dataset
+        #only the most update repo will be avaliable from dowload
+        if data == "SINASC":
+            self.data_sus.cwd(data + "/1996_/Dados/DNRES")
+        elif data == "SIHSUS":
+            self.data_sus.cwd(data + "/200801_/Dados/")
+        elif data == "SIM":
+            self.data_sus.cwd(data + "/1996_/Dados/DNRES")
+
+
+    
+    def attributes(self):
+        print(self.data_sus.dir())
+        
+
+
+     
+        
+            
+
+
+
+#Example
+new_data = dataSus()
+new_data.select_data("SINASC")
+new_data.attributes()
