68eeaf92b0a3cc4984d93761b99caa801eaaa67a
rodriguesmsb <rodriguesmsb@gmail.com>
2019-12-16 15:18:39 -0400

    Change code for Datasus

--- pydbsus.py
+++ pydbsus.py
@@ -1,79 +1,96 @@
 #!/usr/bin/env python3
 # -*- coding:utf8 -*-
 
-# carrega nossa biblioteca fantastica para ftp
+#Load necessary libraries for the Datasus class
 import ftplib as ftp
+import re
+import pandas as pd
 
-# cria a classe para ser instanciada
+
 class Datasus:
+    
+    #Define the main structure of the class
 
-    # aqui é montado o esqueleto da classe
-    def __init__(self, banco, PAGINA = 'ftp.datasus.gov.br',\
+    def __init__(self, banco, PAGINA = 'ftp.datasus.gov.br', \
             PUBLICO = '/dissemin/publicos'):
 
-        self.log = {}
-        self.log['Data'], self.log['Horario'], self.log['Tamanho'],\
-        self.log['Nome'], self.log['Endereco'] = [], [], [], [], []
+        """
+        To start the class user must provide at least one valid name 
+        present in the datasus (e.g to append others repo we just need
+        to add a new PAGINA)
+        """
 
-        '''
-        usei 2 underlines caso a funcao seja usando no shell python
-        pra nao ficar feio quando bater tab mais de uma vez e ficar
-        um monte de coisa a mostra
-        '''
+        self.log = {}
+        self.log['Data'], self.log['Horario'], self.log['Tamanho'], \
+        self.log['Nome'], self.log['Ano'], \
+        self.log['Endereco'] = [], [], [], [], [], []
+        
+
+
         self.__log = []
         self.__pagina = ftp.FTP(PAGINA)
         self.__pagina.login()
         self.__pagina.cwd(PUBLICO)
         self.__banco = banco
 
-    '''
-    essa funcao *TENTA* carregar qualquer banco, afinal esse codigo
-    espera apenas o nome do banco valido dentro do 'publicos'
-    '''
-    def carrega(self):
-        try:
+    
 
+    def load_files(self):
+
+        """
+        This function load files present in the current directory.
+        Right now this took many time to run since it will 'read' all 
+        files in a given repository
+        """
+
+        try:
             self.__pagina.cwd(self.__banco)
             self.__pagina.dir(self.__log.append)
-            self.__magica(self.__log)
+            self.__list_data(self.__log)
 
         except ftp.error_perm:
             print ('diretorio invalido')
 
-    '''
-    essa função faz a magica buscando os bancos. diferente da forma
-    anterior, essa funcao carrega tudo e depois espera ser chamado...
-    a forma anterior era muito ruim
-    '''
 
-    def __magica(self, lista):
+    def __list_data(self, lista):
+
+        """
+        Structure the information avaliable in the repo.
+        """
 
         for i in lista:
             if i.split()[3].endswith(('.dbc','.DBC','.DBF','.dbf')):
-                self.log['Data'].append(i.split()[0]),\
-                self.log['Horario'].append(i.split()[1]),\
-                self.log['Tamanho'].append(i.split()[2]),\
-                self.log['Nome'].append(i.split()[3]),\
+                self.log['Data'].append(i.split()[0]),
+                self.log['Horario'].append(i.split()[1]),
+                self.log['Tamanho'].append(i.split()[2]),
+                self.log['Nome'].append(i.split()[3]),
                 self.log['Endereco'].append(self.__pagina.pwd())
+                if re.search(r"\d+",i.split()[3]):
+                    self.log['Ano'].append(re.findall(r"\d+", i.split()[3])[0])
+                else:
+                    self.log["Ano"].append(None)
 
             elif i.split()[3].endswith(('.dbc','.DBC','.DBF','.dbf')) == False:
                 try:
                     self.__log = []
                     self.__pagina.cwd(i.split()[3])
                     self.__pagina.dir(self.__log.append)
-                    self.__magica(self.__log)
+                    self.__list_data(self.__log)
                     self.__pagina.cwd('..')
                 except:
                     pass
             else:
                 break
 
-    # essa funcao grava em txt com o nome que o usuario escolher
-    def grava(self, nome):
-        if len(a.log['Data']) == 0:
-            print ('Precisa carregar um banco')
-        else:
-            grava = open('{}.txt'.format(nome), 'w+')
-            for x,i in self.log.items():
-                grava.write('{},{}'.format(x, i))
-            grava.close()
+    def write_file(self, path):
+
+        """
+        A function to write 
+        """
+
+        try:
+            file_csv = pd.DataFrame.from_dict(self.log)
+            file_csv.to_csv(path + ".csv", index = False)
+        except:
+            print("No file to write")
+        
