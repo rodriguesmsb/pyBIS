92c1ae02f1a85b7bf3725ed254d5737216a7d386
Marconso <marconsodehsfilho@gmail.com>
2020-01-28 22:15:57 -0400

    cria diretorios e confere sistema

--- pydbui.py
+++ pydbui.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding:utf8 -*-
 
-import sys, time, threading, random
+import sys, time, threading, random, platform, os
 from PyQt5 import QtCore, QtGui, QtWidgets
 from pydbsus import Datasus
+import pandas as pd
 
 class DatasusUi(Datasus):
     def __init__(self, banco=None, PAGINA = 'ftp.datasus.gov.br', \
             PUBLICO = '/dissemin/publicos'):
         super().__init__(self)
 
-        self.select = 0
         self.app = QtWidgets.QApplication(sys.argv)
         self.win = QtWidgets.QMainWindow()
         self.font = QtGui.QFont()
@@ -40,7 +40,7 @@ class DatasusUi(Datasus):
 
         self.downCsv = QtWidgets.QPushButton(self.win)
         self.downCsv.setText('Download .csv')
-        self.downCsv.clicked.connect(self.downcsv)
+      # self.downCsv.clicked.connect(self.downcsv)
         self.downCsv.setFont(self.font)
         self.downCsv.setGeometry(QtCore.QRect(290, 120, 120, 50))
 
@@ -58,34 +58,76 @@ class DatasusUi(Datasus):
 
     def gencsv(self):
         if self.sim.isChecked() == True:
-            self.mygambiarra(self.sim.text())
+            self.startThreads(self.sim.text())
 
         elif self.sinan.isChecked() == True:
-            self.mygambiarra(self.sinan.text())
+            self.startThreads(self.sinan.text())
 
         elif self.sinasc.isChecked() == True:
-            self.mygambiarra(self.sinasc.text())
+            self.startThreads(self.sinasc.text())
 
         else:
             print ('Maque algo')
 
-    def downcsv(self):
+    def startThreads(self, name):
+        dado_bruto = Datasus(banco=f'{name}')
+        
+        self.thread_1 = threading.Thread(target = \
+                dado_bruto.load_files)
 
-        if self.select != 0:
-            Datasus.write_file(self.data)
-        else:
-            print ('Nenhum dado gerado')
+        self.thread_1.start()
 
-    def mygambiarra(self, data):
-        self.data = data
-        self.threads = [Datasus(self.data).load_files, self.statusProgressBar, self.textProgressBar]
+        self.threads = [self.statusProgressBar,\
+                self.textProgressBar]
 
         for i in self.threads:
             threading.Thread(target=i).start()
 
+        while self.thread_1.is_alive() == True:
+            time.sleep(0.5)
+
+            if self.thread_1.is_alive() == False:
+                self.file_csv = pd.DataFrame.from_dict(dado_bruto.log)
+
+                if platform.system().lower() == 'linux':
+                    self.sysLinux(f'{name}')
+
+                elif platform.system().lower() == 'windows':
+                    self.sysWindows(f'{name}')
+
+                break
+            else:
+                pass
+
+    def sysLinux(self, name):
+        diretorio = os.path.expanduser('~/Documentos/')
+        try:
+            os.mkdir(diretorio + 'files_csv/')
+            self.file_csv.to_csv(f'{diretorio}/files_csv/'\
+                    + name.lower() \
+                     + '_linux' + '.csv', index=False)
+
+        except FileExistsError:
+            self.file_csv.to_csv(f'{diretorio}/files_csv/' \
+                    + name.lower() \
+                     + '_linux' + '.csv', index=False)
+
+    def sysWindows(self, name):
+        diretorio = os.path.expanduser('~\\Meus Documentos\\')
+        try:
+            os.mkdir(diretorio + 'files_csv\\')
+            self.file_csv.to_csv(f'{diretorio}\\files_csv\\'\
+                    + name.lower() \
+                    + '_windows' + '.csv', index=False)
+        except FileExistsError:
+            pass
+            self.file_csv.to_csv(f'{diretorio}\\files_csv\\' \
+                    + name.lower() \
+                    + '_windows' + '.csv', index=False)
+
     def statusProgressBar(self):
         n = 0
-        while threading.active_count() == 3:
+        while self.thread_1.is_alive() == True:
             if n < 100:
                 self.progressbar.setValue(n)
                 n += 1
@@ -94,15 +136,15 @@ class DatasusUi(Datasus):
                 n = 0
 
         self.progressbar.setValue(100)
-        self.select = 1
         print ('Dados preparados')
 
     def textProgressBar(self):
         label = ['Carregando...','Isso pode demorar vários minutos', \
                 'Tenha Paciência','Porcentagem referente aos diretórios']
+
         n = 0
-        while threading.enumerate() == 3:
-            time.sleep(1)
+        while self.thread_1.is_alive() == True:
+            time.sleep(1.5)
             self.label.setText(random.choice(label))
 
         self.label.setText('Ja deve estar concluido')
