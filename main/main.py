807604ea1afabf3c5ef93c41b9b3f013efa27adf
marconso <marconsodehsfilho@gmail.com>
2020-11-26 14:20:39 -0400

    barra lateral bonita e funcional

--- main/main.py
+++ main/main.py
@@ -1,9 +1,48 @@
 #!/usr/bin/env python3
 
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QTabWidget,
-                             QTableWidgetItem, QFileDialog)
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QTabWidget, QTabBar,
+                             QTableWidgetItem, QFileDialog, QStylePainter,
+                             QStyle, QStyleOptionTab)
+from PyQt5.QtCore import QRect, QPoint
 from pandas_profiling import ProfileReport
 import qdarkstyle
+
+
+class TabBar(QTabBar):
+    def tabSizeHint(self, index):
+        s = QTabBar.tabSizeHint(self, index)
+        s.transpose()
+        return s
+
+    def paintEvent(self, event):
+        painter = QStylePainter(self)
+        opt = QStyleOptionTab()
+
+        for i in range(self.count()):
+            self.initStyleOption(opt, i)
+            painter.drawControl(QStyle.CE_TabBarTabShape, opt)
+            painter.save()
+
+            s = opt.rect.size()
+            s.transpose()
+            r = QRect(QPoint(), s)
+            r.moveCenter(opt.rect.center())
+            opt.rect = r
+
+            c = self.tabRect(i).center()
+            painter.translate(c)
+            painter.rotate(90)
+            painter.translate(-c)
+            painter.drawControl(QStyle.CE_TabBarTabLabel, opt)
+            painter.restore()
+
+
+class TabWidget(QTabWidget):
+    def __init__(self, *args, **kwargs):
+        QTabWidget.__init__(self, *args, **kwargs)
+        # super().__init__(*args, **kwargs)
+        self.setTabBar(TabBar(self))
+        self.setTabPosition(QTabWidget.West)
 
 
 class Pydbsus_gui(QMainWindow):
@@ -13,8 +52,8 @@ class Pydbsus_gui(QMainWindow):
 
         self.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5'))
         self.setWindowTitle('pyBiss')
-        self.tabs = QTabWidget()
-        self.tabs.setTabPosition(QTabWidget.West)
+        self.tabs = TabWidget()
+        # self.tabs.setTabPosition(QTabWidget.West)
 
         for aba in abas:
             self.tabs.addTab(aba, aba.windowTitle())
