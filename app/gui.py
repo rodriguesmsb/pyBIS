1c30c08f9a1b4717bc8e6ac861b0bf60622908a3
marconso <marconsodehsfilho@gmail.com>
2021-04-05 11:23:06 -0400

    up

--- app/gui.py
+++ app/gui.py
@@ -513,7 +513,8 @@ class DownloadUi(QMainWindow):
 
     def read_file(self):
         def unionAll(dfs):
-            return reduce(lambda df1,df2: df1.union(df2.select(df1.columns)), dfs)
+            return reduce(lambda df1,df2: df1.union(df2.select(df1.columns)),
+                          dfs)
 
         with open(conf + "config.json", "r", encoding='utf8') as f:
             data = json.load(f)
@@ -522,14 +523,11 @@ class DownloadUi(QMainWindow):
             )
             self.spark = start_spark(conf)
             try:
-                self.lista_spark = []
-                for banco in self.files:
-                    self.lista_spark.append(
-                        self.spark.read.csv(banco, header=True)
-                    )
-
+                self.lista_spark = [
+                    self.spark.read.csv(csv, header=True)
+                    for csv in self.files
+                ]
                 self.df = unionAll(self.lista_spark)
-                # self.df = self.spark.read.csv(self.files, header=True)
                 self.write_table()
             except:
                 self.signal_error.emit(
